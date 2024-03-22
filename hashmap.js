22056e6ccc43ca5725e444985a7bd74048e40b3e
tanat8350 <55195958+tanat8350@users.noreply.github.com>
2024-03-22 22:08:46 +0700

    init

---
+++ hashmap.js
@@ -0,0 +1,14 @@
+if (index < 0 || index >= buckets.length) {
+  throw new Error('Trying to access index out of bound');
+}
+
+function hash(key) {
+  let hashCode = 0;
+
+  const primeNumber = 31;
+  for (let i = 0; i < key.length; i++) {
+    hashCode = primeNumber * hashCode + key.charCodeAt(i);
+  }
+
+  return hashCode;
+}
